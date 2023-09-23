726d2ee4b772c7b9af19acc111ee9eb711c013a6
Thomas Elliott <thomas@datch.io>
2023-09-23 22:15:23 +1200

    Set up controller to listen to pod and deployment changes

--- api/v1alpha1/monitor_types.go
+++ api/v1alpha1/monitor_types.go
@@ -20,16 +20,12 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // MonitorSpec defines the desired state of Monitor
 type MonitorSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of Monitor. Edit monitor_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	WebhookEndpoint string `json:"webhookEndpoint"`
+	APIKey          string `json:"apiKey"`
 }
 
 // MonitorStatus defines the observed state of Monitor
--- internal/controller/monitor_controller.go
+++ internal/controller/monitor_controller.go
@@ -18,13 +18,16 @@ package controller
 
 import (
 	"context"
+	deployv1alpha1 "github.com/thomas-elliott/k8s-deploy-monitor/api/v1alpha1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	deployv1alpha1 "github.com/thomas-elliott/k8s-deploy-monitor/api/v1alpha1"
 )
 
 // MonitorReconciler reconciles a Monitor object
@@ -36,6 +39,8 @@ type MonitorReconciler struct {
 //+kubebuilder:rbac:groups=deploy.deploy-monitor.local,resources=monitors,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=deploy.deploy-monitor.local,resources=monitors/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=deploy.deploy-monitor.local,resources=monitors/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -47,9 +52,60 @@ type MonitorReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *MonitorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	var monitorInstance deployv1alpha1.Monitor
+	if err := r.Get(ctx, types.NamespacedName{Name: "monitor-config", Namespace: "default"}, &monitorInstance); err != nil {
+		// handle error
+		log.Error(err, "Failed to retrieve Monitor")
+	}
+
+	endpoint := monitorInstance.Spec.WebhookEndpoint
+	apiKey := monitorInstance.Spec.APIKey
+	log.Info("Processing Monitor", "name", monitorInstance.Name, "endpoint", endpoint, "apiKey", apiKey)
+
+	var pod corev1.Pod
+	if err := r.Get(ctx, req.NamespacedName, &pod); err != nil {
+		if !errors.IsNotFound(err) {
+			log.Error(err, "Failed to retrieve Pod")
+			return ctrl.Result{}, err
+		}
+
+		// If the error indicates the resource isn't a Pod, try fetching a Deployment
+		var deployment appsv1.Deployment
+		if err := r.Get(ctx, req.NamespacedName, &deployment); err != nil {
+			if errors.IsNotFound(err) {
+				log.Info("Neither Pod nor Deployment found, might have been deleted")
+				return ctrl.Result{}, nil
+			}
+			log.Error(err, "Failed to retrieve Deployment")
+			return ctrl.Result{}, err
+		}
+
+		return r.handleDeployment(ctx, &deployment)
+	}
+
+	return r.handlePod(ctx, &pod)
+}
+
+func (r *MonitorReconciler) handlePod(ctx context.Context, pod *corev1.Pod) (ctrl.Result, error) {
+	// TODO: So far the pods haven't been needed, leaving it behind for now
+	log := log.FromContext(ctx)
+
+	status := pod.Status
+	log.Info("Processing Pod", "name", pod.Name, "status", status)
+
+	return ctrl.Result{}, nil
+}
+
+// Separate handling logic for Deployments into its own method
+func (r *MonitorReconciler) handleDeployment(ctx context.Context, deployment *appsv1.Deployment) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+
+	readyReplicas := deployment.Status.ReadyReplicas
+	desiredReplicas := deployment.Status.Replicas
+	image := deployment.Spec.Template.Spec.Containers[0].Image
+	log.Info("Processing Deployment", "name", deployment.Name, "readyReplicas", readyReplicas, "desiredReplicas", desiredReplicas, "image", image)
 
 	return ctrl.Result{}, nil
 }
@@ -57,6 +113,7 @@ func (r *MonitorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 // SetupWithManager sets up the controller with the Manager.
 func (r *MonitorReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&deployv1alpha1.Monitor{}).
+		For(&appsv1.Deployment{}).
+		Owns(&corev1.Pod{}). // TODO: This isn't watching the pods
 		Complete(r)
 }
